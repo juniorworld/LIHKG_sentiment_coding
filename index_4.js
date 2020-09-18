45a0f9a45896ec96e8a2d13fa16e0912374211ac
junior_zhu <yunerzhu@gmail.com>
2020-09-19 00:29:57 +0800

    Update index_4.js

--- index_4.js
+++ index_4.js
@@ -3,7 +3,8 @@ Survey
     .applyTheme("modern");
 
 var json = {
-    surveyId: 'bffce241-70af-4f92-ac6e-f38d886f9d1d'
+    surveyId: 'bffce241-70af-4f92-ac6e-f38d886f9d1d';
+    surveyPostId:'c055ffe6-8ed9-4054-bc72-9362408305cc'
 };
 
 window.survey = new Survey.Model(json);
